4eb26ad9daec7a1e566da07e1810d374664885d1
Frank <frankfax@hotmail.com>
2016-06-02 13:03:16 -0700

    Image Gallery

---
+++ ImageGallery/WebForm1.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+
+namespace webform.ImageGallery
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadImages();
+        }
+
+        private void LoadImages()
+        {
+            foreach (string strfile in Directory.GetFiles(Server.MapPath("~/ImageGallery/Data")))
+            {
+                ImageButton imageButton = new ImageButton();
+                FileInfo fi = new FileInfo(strfile);
+                imageButton.ImageUrl = "~/ImageGallery/Data/" + fi.Name;
+                imageButton.Height = Unit.Pixel(100);
+                imageButton.Width = Unit.Pixel(100);
+                imageButton.Style.Add("padding", "5px");
+                imageButton.Click += new ImageClickEventHandler(imageButton_Click);
+                Panel1.Controls.Add(imageButton);
+            }
+        }
+
+        void imageButton_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("Webform2.aspx?imageURL=" + ((ImageButton)sender).ImageUrl);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+	{
+                string fileName = FileUpload1.FileName;
+                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/ImageGallery/Data/") + fileName);
+		 
+	}
+            Response.Redirect("webform1.aspx");
+        }
+    }
+}
---
+++ ImageGallery/WebForm2.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="webform.ImageGallery.WebForm2" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+<asp:Button ID="Button2" Text="Back to Gallery" runat="server" 
+            onclick="Button2_Click"  />
+<br /><br />
+<asp:Image ID="Image1" Width="800px" Height="550px" runat="server" />
+<br /><br />
+<asp:Button ID="Button1" Text="Back to Gallery" runat="server" 
+            onclick="Button1_Click"  />
+
+    </div>
+    </form>
+</body>
+</html>
